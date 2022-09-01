dfcaa57a34607231101142e8d603f03736f0c069
Loc Nguyen <loc.nguyen@aensmart.com>
2022-09-01 12:05:37 +0700

    update upload file

--- api/utils/files.php
+++ api/utils/files.php
@@ -4,15 +4,22 @@ function saveUploadedFile($file, $targetDir)
 {
     global $FAILED_UPLOADE_FILE_ERROR_CODE;
 
+    $targetFile = $targetDir . basename($file["name"]);
+    
+    if (file_exists($targetFile)){
+        unlink($targetFile);
+    }
+   
+
     if (!file_exists($targetDir)) {
         mkdir($targetDir, 0777, true);
     }
 
-    $targetFile = $targetDir.basename($file["name"]);
+
 
     if (move_uploaded_file($file["tmp_name"], $targetFile)) {
         return "";
-    } 
-    
+    }
+
     return errorResponse($FAILED_UPLOADE_FILE_ERROR_CODE);
 }
