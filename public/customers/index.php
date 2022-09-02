b06db3bbb992151625b84e4123d21c68150b975a
BaoTH <baoth@vitalify.asia>
2022-09-02 22:24:47 +0700

    change page title

--- public/customers/index.php
+++ public/customers/index.php
@@ -3,7 +3,7 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Vendor</title>
+      <title>Customer</title>
       <link href="../css/bootstrap.min.css" rel="stylesheet">
       <link rel="stylesheet" href="../css/customers.css">
    </head>
