add2d4ecef4bfea1e24f1d69ea7e23b4a99c831d
BaoTH <baoth@vitalify.asia>
2022-09-02 12:53:40 +0700

    add tab

--- public/js/vendors.js
+++ public/js/vendors.js
@@ -1,11 +1,55 @@
 const host = "http://php-online-shopping-backend.herokuapp.com/api/";
+const maxItemPerPage = 2;
+let currentPage = 1;
+let vendors = [];
+
+const openSection = (evt, sectionName) => {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  console.log(tablinks);
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+
+  document.getElementById(sectionName).style.display = "block";
+  evt.currentTarget.className += " active";
+};
+
+function prevPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    changePage(currentPage);
+  }
+}
+
+function nextPage() {
+  if (currentPage < numPages()) {
+    current_page++;
+    changePage(current_page);
+  }
+}
+
+const renderPagination = (items) => {
+  const paginationList = document.getElementById("pagination");
+  //   <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+  //   <li class="page-item"><a class="page-link" href="#">1</a></li>
+  //   <li class="page-item"><a class="page-link" href="#">2</a></li>
+  //   <li class="page-item"><a class="page-link" href="#">3</a></li>
+  //   <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  //   return ``
+};
 
 async function callAPI(method, url, onSuccess) {
   const xhttp = new XMLHttpRequest();
   const apiUrl = `${host}${url}`;
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      console.log(this);
       const responseJsonObj = JSON.parse(this.responseText);
       onSuccess(responseJsonObj);
     }
@@ -15,7 +59,6 @@ async function callAPI(method, url, onSuccess) {
 }
 
 function renderItem(item, index) {
-  console.log({ item });
   const {
     CreatedAt,
     UpdatedAt,
@@ -25,9 +68,7 @@ function renderItem(item, index) {
     Name,
     ProductID,
   } = item;
-  //   const duration = moment.duration(end.diff(startTime));
-  //   var hours = duration.asHours();
-  //   console.log(duration);
+
   return `<div class="col">
   <div class="card shadow-sm">
     <div>
@@ -47,26 +88,71 @@ function renderItem(item, index) {
 </div>`;
 }
 
+function changePage(page) {
+  console.log("CHANGE PAGE");
+  var btn_prev = document
+    .getElementById("btn_prev")
+    .addEventListener("click", prevPage);
+  var btn_next = document
+    .getElementById("btn_next")
+    .addEventListener("click", nextPage);
+  //   btn_next.addEventListener("click", nextPage, false);
+
+  var page_span = document.getElementById("page");
+
+  // Validate page
+  if (page < 1) page = 1;
+  if (page > numPages()) page = numPages();
+
+  const list = [];
+  for (
+    var i = (page - 1) * maxItemPerPage;
+    i < page * maxItemPerPage && i < vendors.length;
+    i++
+  ) {
+    if (vendors[i]) {
+      list.push(vendors[i]);
+    }
+  }
+  renderList(list);
+
+  //   if (page == 1) {
+  //     btn_prev.style.visibility = "hidden";
+  //   } else {
+  //     btn_prev.style.visibility = "visible";
+  //   }
+
+  //   if (page == numPages()) {
+  //     btn_next.style.visibility = "hidden";
+  //   } else {
+  //     btn_next.style.visibility = "visible";
+  //   }
+}
+
+function numPages() {
+  return Math.ceil(vendors / maxItemPerPage);
+}
+
 function renderList(list) {
+  console.log({ list });
   let html = list.map(renderItem).join("");
-  document.getElementById("vendorsList").innerHTML = html;
+  document.getElementById("products").innerHTML = html;
 }
 
 async function index() {
   try {
     const onSuccess = (response) => {
-      console.log({ response });
-      renderList(response);
+      if (Array.isArray(response)) {
+        vendors = response;
+        renderList(vendors);
+        // changePage(1);
+        renderPagination(response);
+      }
     };
     await callAPI("GET", "/api/product", onSuccess);
   } catch (err) {
     console.log(err);
   }
-}
-
-async function getVendors(method, url) {
-  try {
-  } catch (err) {}
 }
 
 index();
--- public/vendors/index.php
+++ public/vendors/index.php
@@ -5,21 +5,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Vendor</title>
     <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/vendors.css">
   </head>
   <body>
     <?php include "../template/headers/admin_header.php"; ?>
       <div class="container">
-        <div class="album py-5 bg-light">
-          <div id="vendorsList" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"> 
+        <!-- Tabs -->
+      <ul class="nav nav-pills">
+  <li class="nav-item " >
+    <a onclick="openSection(event, 'products')"  class="nav-link tablinks active" href="#">Products</a>
+  </li>
+  <li class="nav-item ">
+    <a  onclick="openSection(event, 'create-products')" class="nav-link tablinks" href="#">Add new product</a>
+  </li>
+</ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+   
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+        <!-- Section -->
+        <div class="album py-5  ">
+          <div id="products" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 tabcontent"> 
             
           </div>
+          <div id="create-products" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 tabcontent"> 
+            
+            </div>
+          <nav aria-label="Page navigation example">
+      <div>
+    <!-- <ul class="pagination">
+    <li id="btn_prev" class="page-item"><a class="page-link">Previous</a></li>
+    <li id="btn_next" class="page-item"><a class="page-link">Next</a></li>
+    </ul> -->
+      </div>
+ 
+</nav>
         </div>
       </div>
     <?php include "../template/footers/admin_footer.php"; ?>  
-    <script src="moment.js"></script>
     <script src="../js/bootstrap.bundle.min.js"></script>
     <script src="../const/apiServices.js"></script> 
-    <script type="module" src="../js/vendors.js"></script>
+    <script src="../js/vendors.js"></script>
     
   </body>
 </html>
