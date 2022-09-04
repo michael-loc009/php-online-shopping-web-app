5c0a0f81ef42aa6ee9bfddfa3f33103b96666b0e
Loc Nguyen <loc.nguyen@aensmart.com>
2022-09-04 16:50:35 +0700

    modify default headers

--- api/index.php
+++ api/index.php
@@ -29,10 +29,10 @@ $shopDb = (new DatabaseConnector())->getShopDbConnection();
 $accountDb = (new DatabaseConnector())->getAccountDbConnection();
 
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Allow-Headers: Origin, Accept, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, X-Auth-Token");
+header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Origin, Accept, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $requestMethod = $_SERVER["REQUEST_METHOD"];
